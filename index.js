3fe09cbcf8ffa6ebdc8940997e68242aeadc40d5
Penha Vasyl <vasylpenha@gmail.com>
2021-02-07 20:30:44 +0200

    Optimiz code, add slider, fix bugs, optimiz images.

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  document.body.classList.add('loaded');
+}
+	
+$('.sliders').slick({
+  prevArrow: "<img src='img/arrow-left.png' class='prev' alt='1'>",
+  nextArrow: "<img src='img/arrow-right.png' class='next' alt='2'>",
+});
